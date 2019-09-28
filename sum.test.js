dce05926c139401c9e159613d3060e725ca27c74
zhoususu <431185521@qq.com>
2019-09-28 20:46:50 +0800

    #update: 1

--- sum.test.js
+++ sum.test.js
@@ -2,5 +2,9 @@ const sum = require('./sum.js');
 
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 4 to equal 6', () => {
+  expect(sum(2, 4)).toBe(6);
+})
